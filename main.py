d0feb1bfde954fa1800ab3dcba7c8319c11671e7
dingdang66686 <dingdang66686@outlook.com>
2020-05-13 19:30:29 +0800

    修复读取文件bug并添加结束界面

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ from DataProcessing import init_data
 from Stopwords import init_Stopwords
 from Stopwords import db_search_Stopwords
 import tkinter
-from tkinter import scrolledtext,filedialog
+from tkinter import scrolledtext,filedialog,messagebox
+
+
 
 def process(article):
+    button1.config(state='disabled')
+    button2.config(state='disabled')
     init_data(article)
     init_Stopwords()
+    result = '我是中心句（假的）'
+    messagebox.showinfo(title='找到中心句',message=result)
+    button1.config(state='active')
+    button2.config(state='active')
 
 def from_input():
     process(textbox.get('0.0','end'))
 def from_file():
-    with filedialog.askopenfile(title="上传文件", filetypes=[("文本文件", ".txt")]) as f:
+    with filedialog.askopenfile('r',title="上传文件", filetypes=[("文本文件", ".txt")]) as f:
+        textbox.delete(1.0, 'end')
+        textbox.insert(1.0, f.read())
         process(f.read())
 
 # 主窗口
@@ -21,7 +31,7 @@ window.geometry('800x600')
 window.resizable(width=False,height=False)
 window.iconbitmap('icon.ico')
 
-tkinter.Label(window,text='请输入文章：',font=('微软雅黑',14)).place(x=10,y=10,anchor='nw')
+tkinter.Label(window,text='请输入文章（英文）：',font=('微软雅黑',14)).place(x=10,y=10,anchor='nw')
 textbox = scrolledtext.ScrolledText(window,font=('微软雅黑',14))
 textbox.place(x=10,y=40,anchor='nw',width = 780,height = 500)
 
